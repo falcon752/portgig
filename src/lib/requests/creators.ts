d7a0c3628a0c1a2dd48636626b877651445a9f75
falcon752 <atikuquadrisegun@gmail.com>
2025-12-24 18:14:58 +0100

    dropdown fix4

--- src/lib/requests/creators.ts
+++ src/lib/requests/creators.ts
@@ -61,19 +61,25 @@ export interface Creator {
   portfolio: {
     template_type: string;
   };
-profile: {
-  profile_picture: string;
-  years_of_experience: string; // or number if your API returns a number
-  field: string;
-  bio: string;
-  industry: string;
-  location: {
-    state: string;
-    lga: string;
-    _id: string;
+  profile: {
+    profile_picture: string;
+    years_of_experience(years_of_experience: any): import("react").ReactNode;
+    field: ReactNode;
+    location: any;
+    bio: string;
+    profile: {
+      profile_picture: string;
+      years_of_experience: string;
+      field: string;
+      bio: string;
+      industry: string;
+      location: {
+        state: string;
+        lga: string;
+        _id: string;
+      };
+    };
   };
-};
-
   resume: {
     skills: string[];
     other_skills: string[];
