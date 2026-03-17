2afc672e6ce549fe556148ac17c2dfd70a89892a
falcon752 <atikuquadrisegun@gmail.com>
2026-03-17 21:59:00 +0100

    fix: remove stray brace in TemplateOneAboutme

--- src/components/template-1/TemplateOneAboutme.tsx
+++ src/components/template-1/TemplateOneAboutme.tsx
@@ -64,7 +64,7 @@ export const TemplateOneAboutme = ({ portfolio }: TemplateOneAboutmeProps) => {
   return (
     <section className="h-fit bg-black" style={customBackgroundColor ? { backgroundColor: customBackgroundColor } : undefined}>
       <div className="max-w-[1400px] mx-auto px-4 sm:px-6 md:px-8 lg:px-10 pt-10 pb-5 flex flex-col gap-6">
-        {/* About Me */}}
+        {/* About Me */}
         <div className={cardStyle}>
           <p
             className={`${responsiveTextStyle} text-[12px] sm:text-[16px] md:text-[16px] lg:text-[16px] leading-[140%] sm:leading-[140%] text-center max-w-3xl`}
